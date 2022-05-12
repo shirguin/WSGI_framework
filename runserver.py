2752bdc7a7131e70323e0d82e9686bab3fd8b395
shirguin <shirguin@yandex.ru>
2022-05-12 16:08:13 +0300

    доделать статику

---
+++ runserver.py
@@ -0,0 +1,11 @@
+from wsgiref.simple_server import make_server
+
+from wsgi_framework.main import Framework
+from urls import routes, fronts
+
+
+application = Framework(routes, fronts)
+
+with make_server('', 8000, application) as httpd:
+    print('Сервер запущен на порту 8000...')
+    httpd.serve_forever()
