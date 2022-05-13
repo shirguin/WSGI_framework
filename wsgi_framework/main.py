14524752deded46d47d1d3d5a36244cfd6b72890
shirguin <shirguin@yandex.ru>
2022-05-13 15:29:23 +0300

    Нужно распарсить POST запрос

--- wsgi_framework/main.py
+++ wsgi_framework/main.py
@@ -7,6 +7,13 @@ class Framework:
         self.lsFronts = fronts_obj_ls
 
     def __call__(self, environ, start_response):
+        method = environ['REQUEST_METHOD']
+        query_string = environ['QUERY_STRING']
+        print(f'Метод запроса:{method}:{query_string}')
+
+        request_params = self.parse_input_data(query_string)
+        print(f'Список Request_Params: {request_params}')
+
         # Адрес перехода
         path = environ['PATH_INFO']
 
@@ -29,6 +36,15 @@ class Framework:
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    def parse_input_data(self, data: str):
+        result = {}
+        if data:
+            params = data.split('&')
+            for item in params:
+                key, value = item.split('=')
+                result[key] = value
+        return result
 
 
 class PageNotFound404:
