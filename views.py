38437324e4771c0af58075ad553d99ae02eafbcc
Shirguin <shirguin@yandex.ru>
2022-05-24 08:24:23 +0300

    Доделать CopyCourse

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from wsgi_framework.templator import render
 
 site = Engine()
 
+
 class Index:
     def __call__(self, request):
         context = {
@@ -52,21 +53,120 @@ class Page:
 # Создать категорию
 class CreateCategory:
     def __call__(self, request):
+
         if request['method'] == 'POST':
-            pass
-            # data = request['data']
-            # name_category = data['name_category']
-            # name_category = site.decode_value(name_category)
-            #
-            # category_id = data.get('category_id')
-            #
-            # category = None
-            # if category_id:
-            #     category = site.find_category_by_id(int(category_id))
-            #
-            # new_category = site.create_category(name_category)
-            # site.categories.append(new_category)
-            # return '200 OK', render('index.html', objects_list=site.categories)
+            print('IF POST')
+            data = request['data']
+            name_category = data['name_category']
+            name_category = site.decode_value(name_category)
+
+            category_id = data.get('category_id')
+
+            category = None
+            if category_id:
+                category = site.find_category_by_id(int(category_id))
+
+            new_category = site.create_category(name_category)
+            site.categories.append(new_category)
+
+            context = {
+                'title': 'Главная',
+                'request': request,
+                'categories': site.categories
+            }
+            return '200 OK', render('index.html', context=context, encoding='utf-8')
         else:
-            categories = site.categories
-            return '200 OK', render('create_category.html', categories=categories)
+            context = {
+                'title': 'Создание категории',
+                'request': request,
+                'categories': site.categories
+            }
+            return '200 OK', render('create_category.html', context=context)
+
+
+# Список курсов
+class CoursesList:
+    def __call__(self, request):
+        try:
+            category = site.find_category_by_id(int(request['request_params']['id']))
+            context = {
+                'title': 'Список курсов',
+                'request': request,
+                'objects_list': category.courses,
+                'category': category.category,
+                'id': category.id
+            }
+            return '200 OK', render('course-list.html', context=context)
+        except KeyError:
+            return '200 OK', 'Курсы еще не добавлены'
+
+
+# Создать курс
+class CreateCourse:
+    def __call__(self, request):
+        if request['method'] == 'POST':
+            data = request['data']
+            name_course = data['name_course']
+            name_course = site.decode_value(name_course)
+
+            category = site.find_category_by_id(int(self.category_id))
+
+            course = site.create_course('interactive', name_course, category)
+            site.courses.append(course)
+            context = {
+                'title': 'Список курсов',
+                'request': request,
+                'objects_list': category.courses,
+                'category': category.category,
+                'id': category.id
+            }
+            return '200 OK', render('course-list.html', context=context)
+        else:
+            try:
+                self.category_id = int(request['request_params']['id'])
+                category = site.find_category_by_id(int(self.category_id))
+                context = {
+                    'title': 'Создание курса',
+                    'request': request,
+                    'category': category.category,
+                    'id': category.id
+                }
+                return '200 OK', render('create-course.html', context=context)
+            except KeyError:
+                return '200 OK', 'Категории еще не добавлены'
+
+
+# Список категорий
+class CategoryList:
+    pass
+
+
+# Копирование курса
+class CopyCourse:
+    def __call__(self, request):
+        request_params = request['request_params']
+
+        try:
+            name_course = request_params['name_course']
+            old_course = site.get_course(name_course)
+            category = old_course.category
+            if old_course:
+                new_name_course = f'copy_{name_course}'
+                new_course = old_course.clone()
+                new_course.name = new_name_course
+                site.courses.append(new_course)
+                new_course.category.courses.append(new_course)
+                context = {
+                    'title': 'Список курсов',
+                    'request': request,
+                    'categories': site.categories,
+                    # 'objects_list': category.courses,
+                    'objects_list': site.courses,
+                    'category': category,
+                    'id': category.id
+                }
+                return '200 OK', render('course-list.html', context=context)
+
+        except KeyError:
+            return '200 Ok', 'Курсы еще не добавлены'
+
